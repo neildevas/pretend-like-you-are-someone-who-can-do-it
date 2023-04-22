ec0c2c5a17cc93a27ffc1ec89a469e45d84be547
Neil Devas <neil.devas97@gmail.com>
2023-04-22 17:57:20 -0400

    button that shows or hdies quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
+import {useReducer} from "react";
 
 const quotes = [{
-  quote: "Pretend like you are someone who can do it", author: "Neil Gaiman",
+  quote: "Pretend like you are someone who can do it",
+  author: "Neil Gaiman",
 }, {
   quote: "Pessimism is an indulgence, orthodoxy is the enemy of invention, despair an insult to the imagination",
   author: "Wade Davis"
@@ -12,16 +14,15 @@ const quotes = [{
   quote: "Often when you think you're at the end of something, you're at the beginning of something else",
   author: "Fred Rogers"
 }, {
-  quote: "Anything worth doing is worth doing slowly",
-  author: "Mae West"
-}, {
   quote: "Hard choices, easy life. Easy choices, hard life.", author: "Jerzy Gregorek"
 }];
 
 function App() {
+  const [showAllQuotes, toggleShowAllQuotes] = useReducer(val => !val, false);
+  const visibleQuotes = showAllQuotes ? quotes : [quotes[0]];
   return (
     <div className="App">
-      {quotes.map(({ quote, author }, index) => (
+      {visibleQuotes.map(({ quote, author }, index) => (
         <div className={"card"} key={index}>
           <p>
             {quote}
@@ -31,6 +32,9 @@ function App() {
           </p>
         </div>
       ))}
+      <button onClick={toggleShowAllQuotes}>
+        {showAllQuotes ? 'Less' : 'More'} Quotes
+      </button>
     </div>
   )
 }
