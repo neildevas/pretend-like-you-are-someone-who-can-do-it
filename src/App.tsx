bdd7173ee8c8c63f0b73ee12fa6b434261ffe420
Neil Devas <neil.devas97@gmail.com>
2023-04-22 17:36:15 -0400

    add more quotes'

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
-import { useState } from 'react'
 import './App.css'
 
-const quotes = [{ quote: "Pretend like you are someone who can do it", author: "Neil Gaiman" }];
+const quotes = [{
+  quote: "Pretend like you are someone who can do it", author: "Neil Gaiman",
+}, {
+  quote: "Pessimism is an indulgence, orthodoxy is the enemy of invention, despair an insult to the imagination",
+  author: "Wade Davis"
+}, {
+  quote: "When I let go of what I am, I become what I might be",
+  author: "Lao Tzu"
+}, {
+  quote: "Often when you think you're at the end of something, you're at the beginning of something else",
+  author: "Fred Rogers"
+}, {
+  quote: "Anything worth doing is worth doing slowly",
+  author: "Mae West"
+}, {
+  quote: "Hard choices, easy life. Easy choices, hard life.", author: "Jerzy Gregorek"
+}];
 
 function App() {
-  const [quoteIndex, setQuoteIndex] = useState(0)
-  const quote = quotes[quoteIndex];
   return (
     <div className="App">
-      <div className="card">
-        <p>
-          {quote.quote}
-        </p>
-        <p>
-          - {quote.author}
-        </p>
-      </div>
+      {quotes.map(({ quote, author }, index) => (
+        <div className={"card"} key={index}>
+          <p>
+            {quote}
+          </p>
+          <p>
+            - {author}
+          </p>
+        </div>
+      ))}
     </div>
   )
 }
