a1f5d61b9ac272ad97160f74d35dd6c8b118e745
Neil Devas <neil.devas97@gmail.com>
2023-04-22 17:58:35 -0400

    Feature/add quote (#1)
    * add more quotes'
    
    * button that shows or hdies quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import { useState } from 'react'
 import './App.css'
+import {useReducer} from "react";
 
-const quotes = [{ quote: "Pretend like you are someone who can do it", author: "Neil Gaiman" }];
+const quotes = [{
+  quote: "Pretend like you are someone who can do it",
+  author: "Neil Gaiman",
+}, {
+  quote: "Pessimism is an indulgence, orthodoxy is the enemy of invention, despair an insult to the imagination",
+  author: "Wade Davis"
+}, {
+  quote: "When I let go of what I am, I become what I might be",
+  author: "Lao Tzu"
+}, {
+  quote: "Often when you think you're at the end of something, you're at the beginning of something else",
+  author: "Fred Rogers"
+}, {
+  quote: "Hard choices, easy life. Easy choices, hard life.", author: "Jerzy Gregorek"
+}];
 
 function App() {
-  const [quoteIndex, setQuoteIndex] = useState(0)
-  const quote = quotes[quoteIndex];
+  const [showAllQuotes, toggleShowAllQuotes] = useReducer(val => !val, false);
+  const visibleQuotes = showAllQuotes ? quotes : [quotes[0]];
   return (
     <div className="App">
-      <div className="card">
-        <p>
-          {quote.quote}
-        </p>
-        <p>
-          - {quote.author}
-        </p>
-      </div>
+      {visibleQuotes.map(({ quote, author }, index) => (
+        <div className={"card"} key={index}>
+          <p>
+            {quote}
+          </p>
+          <p>
+            - {author}
+          </p>
+        </div>
+      ))}
+      <button onClick={toggleShowAllQuotes}>
+        {showAllQuotes ? 'Less' : 'More'} Quotes
+      </button>
     </div>
   )
 }
